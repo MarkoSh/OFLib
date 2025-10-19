e1ad6ec7643c329b591c22f745f4e89444d05f85
Mark <markhost@yandex.ru>
2025-10-19 10:38:53 +0300

    Updates

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -62,6 +62,25 @@ import PocketBase from '../vendor/pocketbase.es.mjs';
 				})();
 			}
 
+			{
+				(async () => {
+					const response = await fetch(chrome.runtime.getURL('css/style.css'));
+
+					const code = await response.text();
+
+					chrome.userScripts.register([{
+						id: 'OFLibInject3',
+						js: [{
+							code: `console.log('CSS'); debugger; document.head.innerHTML += '<style type="text/css">${code}</style>'`,
+						}],
+						runAt: 'document_start',
+						world: 'MAIN',
+						matches: ['*://onlyfans.com/*'],
+						allFrames: true,
+					}]);
+				})();
+			}
+
 			handlers.map((Handler: any) => {
 				return new Handler($this);
 			});
